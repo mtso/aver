f01734d249bc74f8d8fba3533fc312c998e08486
mtso <mtso@tuta.io>
2017-03-10 10:57:29 -0800

    fmt doc.go

--- doc.go
+++ doc.go
@@ -1,22 +1,22 @@
 // Package aver adds assertion functions for Go. Its usage is inspired by the "expect" assertion pattern.
 //
 // Usage
-// 
+//
 // Example usage in a *_test.go file:
 // 	import (
 // 		"testing"
 // 		"github.com/mtso/aver"
 // 	)
-// 	
+//
 // 	func TestExample(t *testing.T) {
 // 		// Create a new Averrer instance
 // 		aver := aver.New(t)
-//	
+//
 // 		// FAIL the test with Errorf
 // 		expected := "expectedString"
 // 		actual := "actualString"
 // 		aver(expected).ToEqual(actual)
-// 		
+//
 // 		// Store the Aver object in a variable
 // 		isEven := aver(true)
 // 		actual1 := 1 % 2 == 0 // false
@@ -26,14 +26,14 @@
 // 	}
 //
 // Aver is Expect
-// 
+//
 // Aver (Expect) assertions allow you to compare two values in a test.
 // aver.New(t *testing.T) returns a new Averrer function.
 // The Averrer function accepts an "expected" value as a parameter and returns
 // an Aver object that conforms to the Averrable interface. The Aver
 // object stores the expected value and a reference to the testing.T instance.
 // The Aver object can then be called with an Averrable function to compare
-// the stored "expected" value with an "actual" value passed into the 
+// the stored "expected" value with an "actual" value passed into the
 // Averrable function.
-// 
+//
 package aver
