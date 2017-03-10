6b2e9546adf3e8ec757bec24080860ffd5df747a
mtso <mtso@tuta.io>
2017-03-10 11:43:29 -0800

    Add an Averrer type and make aver private

--- aver.go
+++ aver.go
@@ -5,34 +5,43 @@ import (
 	"testing"
 )
 
+// Averrable describes functions that can be called on an averred expected value.
 type Averrable interface {
-	ToEqual(interface{})
-	ToNotEqual(interface{})
+	// Compares a passed actual value with the stored expected value for equality.
+	ToEqual(actual interface{})
+	// Compares a passed actual value with the stored expected value for inequality.
+	ToNotEqual(actual interface{})
 }
 
-func New(t *testing.T) func(actual interface{}) Averrable {
-	return func(actual interface{}) Averrable {
-		return Averrer{
-			actual,
+// Averrer produces a new Averrable from an expected value.
+// Stores the expected value and returns an Averrable.
+type Averrer func(expected interface{}) Averrable
+
+// aver wraps testing.T and stores an expected value for use in Averrable functions.
+type aver struct {
+	expected interface{}
+	*testing.T
+}
+
+// Returns a new Averrer function.
+func New(t *testing.T) Averrer {
+	return func(expected interface{}) Averrable {
+		return aver{
+			expected,
 			t,
 		}
 	}
 }
 
-type Averrer struct {
-	actual interface{}
-	*testing.T
-}
-
-func (a Averrer) ToEqual(expected interface{}) {
-	eq := reflect.DeepEqual(a.actual, expected)
+func (a aver) ToEqual(actual interface{}) {
+	eq := reflect.DeepEqual(a.expected, actual)
 	if !eq {
-		a.Errorf("Expected %q to equal %q", a.actual, expected)
+		a.Errorf("Expected %q to equal %q", a.expected, actual)
 	}
 }
 
-func (a Averrer) ToNotEqual(expected interface{}) {
-	if reflect.DeepEqual(a.actual, expected) {
-		a.Errorf("Expected %q to NOT equal %q", a.actual, expected)
+func (a aver) ToNotEqual(actual interface{}) {
+	if reflect.DeepEqual(a.expected, actual) {
+		a.Errorf("Expected %q to NOT equal %q", a.expected, actual)
 	}
 }
